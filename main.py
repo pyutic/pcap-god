b0838740542f2497b7844d136d351ed934c92a2b
pyutic <pyutic@gmail.com>
2014-08-01 17:19:56 +0000

    main.py with bob_nc.pcap -- parse okay

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print "123"
-print "thisispyutic"
+import dpkt
+
+def ip_decode(p):
+	return ".".join(["%d" % ord(x) for x in str(p)])
+
+name = raw_input("Name => ")
+f = open(name,'rb')
+pcap = dpkt.pcap.Reader(f)
+
+for ts, buf in pcap:
+	eth = dpkt.ethernet.Ethernet(buf)
+	try:
+		ip = eth.data
+		tcp = ip.data
+		print ip_decode(ip.src), ip_decode(ip.dst)
+		print tcp.seq, tcp.ack
+		print tcp.data
+	except:
+		continue
+f.close()
