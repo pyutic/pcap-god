3e80a95c9185ba3b326d25c212fbe98102244872
P3NGU1N <pyutic@P3NGU1Nui-MacBook-Air.local>
2014-08-08 03:06:42 +0900

    checker and server

--- check.py
+++ check.py
@@ -1,3 +1,38 @@
-import os, sys, glob
+import socket
 
-i
+destIP = "localhost"
+destPort = 31337
+sz = 4096
+
+def inout(destIP, destPort, data, f):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((destIP, destPort))
+
+	rt = 1
+	i = 0
+	while(i<len(data)):
+		if(f=="s"):
+			s.send(data[i])
+			f = "r"
+			
+		elif(f=="r"):
+			rcv = s.recv(sz)
+			if(rcv!=data[i]):
+				rt = 0
+				print "-"*20+"Recved"
+				print rcv
+				print "="*20+"Predicted"
+				print data[i]
+			f = "s"
+
+		i = i+1
+	return rt
+
+data = []
+hola = ["1", "2", "1\n"]
+
+for i in hola:
+	data.append(i)
+	data.append(i*2)
+
+inout(destIP, destPort, data, "s")
---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("127.0.0.1", 31337))
+s.listen(5)
+
+conn, addr = s.accept()
+
+for i in xrange(3):
+	data = conn.recv(1024)
+	conn.send(data*3)
+
